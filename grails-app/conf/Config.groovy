fa691891b367f1d78bf98b2a29230cd521eb17e8
Rogerio Luiz Cardoso <rlcsf@cin.ufpe.br>
2015-08-12 12:18:23 -0300

    plugin spring security instalado e modelos User/Role inseridos

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -119,3 +119,20 @@ log4j.main = {
            'org.hibernate',
            'net.sf.ehcache.hibernate'
 }
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'greforco.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'greforco.UserRole'
+grails.plugin.springsecurity.authority.className = 'greforco.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                ['permitAll'],
+	'/index':           ['permitAll'],
+	'/index.gsp':       ['permitAll'],
+	'/assets/**':       ['permitAll'],
+	'/**/js/**':        ['permitAll'],
+	'/**/css/**':       ['permitAll'],
+	'/**/images/**':    ['permitAll'],
+	'/**/favicon.ico':  ['permitAll']
+]
+
