8fd0874ec8bd7dbc3307a2da3d02c613ed6f8509
Fabiano Matos <fgm@cin.ufpe.br>
2015-08-23 21:47:48 -0300

    criada acao sobre no controller home

--- grails-app/controllers/greforco/HomeController.groovy
+++ grails-app/controllers/greforco/HomeController.groovy
@@ -2,8 +2,17 @@ package greforco
 
 class HomeController {
 
-    def index() { }
-    def perfil() {}
+    def index() { 
+
+    }
+    
+    def perfil() {
+
+    }
+
+    def sobre() {
+
+    }
     
   //   def ifAnyGranted = { attrs, body ->
 	 //   System.out.println("${attrs.role}")
