0f49968d02354b166815087c68ec8f3ac11f2514
Fabiano Matos <fgm@cin.ufpe.br>
2015-08-30 04:51:17 -0300

    alterando o construtor, constraints e campos

--- grails-app/domain/greforco/Course.groovy
+++ grails-app/domain/greforco/Course.groovy
@@ -9,21 +9,22 @@ class Course {
 	//String teacher
 	String description
 	String value
-	String evaluation
+	//String evaluation
 	Date dateCreated
 
 
-	Course(String name, String description, String value, Matter matter, String evaluation) {
+	Course(Teacher teacher, String name, String description, String value, Matter matter) {
 		this()
+		this.teacher = teacher
 		this.name = name
 		this.description = description
-		this.evaluation = evaluation
+		//this.evaluation = evaluation
 		this.matter = matter
 		this.value = value 
 	}
 
 
     static constraints = {
-    	teacher(nullable:true, blank:true)
+    	description(nullable:true, blank:true)
     }
 }
